1a84edea54766b7152d295b5b4764a9b8391d8ba
Eder Leao Moosmann <elm@mail.ederlm.de>
2025-12-12 19:32:15 +0100

    Day 2, 2025: not faster, but prettier

--- src/solvers/02_25.zig
+++ src/solvers/02_25.zig
@@ -1,24 +1,33 @@
 const std = @import("std");
 
+fn is_invalid(number:usize, chunk_size: usize) !bool {
+    const base = std.math.pow(usize, 10, chunk_size); 
+    const first_chunk = number % base;
+    var n = number;
+    while (n > 0){
+        const next_chunk = n % base;
+        if (next_chunk != first_chunk){
+            return false;
+        }
+        n = try std.math.divFloor(usize, n , base);
+    }
+    return true;
+}
+
 fn part1(input: []u8) !void {
     var ranges = std.mem.splitAny(u8, input, ","); 
     var invalidNumSum: u64 = 0;
     while (ranges.next()) |range| {
-        if (range.len == 0) continue;
+        if (range.len < 2) continue;
         var parsedRange = std.mem.splitAny(u8, std.mem.trim(u8, range, "\n"), "-");
         const start = try std.fmt.parseInt(u64, parsedRange.next().?, 10);
         const end = try std.fmt.parseInt(u64, parsedRange.next().?, 10);
-
-        var invalidNumbers: std.ArrayList(usize) = .empty;
-        defer invalidNumbers.deinit(std.heap.page_allocator);
         for (start..end+1) |number| {
             const numStr = try std.fmt.allocPrint(std.heap.page_allocator, "{d}", .{number});
-            defer std.heap.page_allocator.free(numStr);
-            if (numStr.len % 2 == 0) {
-                const half = numStr.len / 2;
-                if (std.mem.eql(u8, numStr[0..half], numStr[half..])) {
-                    invalidNumSum += number;
-                    try invalidNumbers.append(std.heap.page_allocator, number);
+            if (numStr.len % 2 == 0){
+                const chunk_size = numStr.len / 2;
+                if (try is_invalid(number, chunk_size)){
+                    invalidNumSum+=number;
                 }
             }
         }
@@ -52,19 +61,10 @@ fn has_sequence(number: usize) !bool {
         }
         max_sequence -= 1;
     }
-    for (possibleSequences.items) |sequence_size| {
-        const expected_sequence = numStr[0..sequence_size];
-        var current_start: usize = sequence_size;
-        while (current_start <= numStr.len-sequence_size) {
-            if (!std.mem.eql(u8, expected_sequence, numStr[current_start..current_start+sequence_size])) {
-                break;
-            }
-            current_start += sequence_size;
-        }
-        if (current_start == numStr.len) {
+    for (possibleSequences.items) |chunk_size| {
+        if (try is_invalid(number, chunk_size)){
             return true;
         }
-
     }
     return false;
 }
@@ -89,7 +89,7 @@ fn part2(input: []u8) !void {
 }
 
 pub fn solve(input: []u8) void {
-    std.debug.print("Solving 01/25\n", .{});
+    std.debug.print("Solving 02/25\n", .{});
     part1(input) catch |err| {
         std.debug.print("Failed to solve problem part 1 {}.", .{err});
         return;
